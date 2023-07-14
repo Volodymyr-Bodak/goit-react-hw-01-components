e507195cf2b02def7fe7f26dc351fb6f2e1db901
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-07-14 13:23:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from 'components/App';
 import './index.css';
-import Profile from 'components/Profile/Profile';
-import Statistics from 'components/Stats/Stats';
-import Friendlist from 'components/Friendslist/Friendlist';
-import Transactions from 'components/Transactions/Trancations';
+
 
 
 
